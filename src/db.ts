51f51a7d18d771818d8227672258e656e75177cf
Ulrich Feindt <ufeindt@gmail.com>
2025-03-16 14:52:26 +0100

    Define Dexie schema and types

---
+++ src/db.ts
@@ -0,0 +1,72 @@
+import Dexie, { type EntityTable } from 'dexie'
+
+interface BaseRecord {
+  createdAt: Date
+  updatedAt: Date
+}
+
+interface Checkout extends BaseRecord {
+  id: number
+  discounts: Discount[]
+  lines: CheckoutLine[]
+}
+
+interface CheckoutLine extends BaseRecord {
+  id: number
+  discounts: Discount[]
+  lineTotal: number
+  quantity: number
+  sku: string
+  unitPrice: number
+  variant: Variant
+}
+
+interface Discount {
+  amount?: number
+  percentage?: number
+  title: string
+}
+
+interface Product extends BaseRecord {
+  id: number
+  title: string
+  variants: Variant[]
+}
+
+interface Sale extends BaseRecord {
+  id: number
+  checkoutId: number
+  discounts: Discount[]
+  lines: CheckoutLine[]
+  paymentId: string
+}
+
+interface StoreConfig extends BaseRecord {
+  id: number
+  storeName: string
+  swishNumber: string
+}
+
+interface Variant extends BaseRecord {
+  id: number
+  price: number
+  sku: string
+  title: string
+}
+
+const db = new Dexie('POSDatabase') as Dexie & {
+  checkout: EntityTable<Checkout, 'id'>
+  products: EntityTable<Product, 'id'>
+  sales: EntityTable<Sale, 'id'>
+  storeConfig: EntityTable<StoreConfig, 'id'>
+}
+
+db.version(1).stores({
+  checkout: '++id, *discounts, *lines',
+  products: '++id, title, *variants',
+  sales: '++id, checkoutId, *discounts, *lines, paymentId',
+  storeConfig: '++id, storeName, swishNumber',
+})
+
+export type { Checkout, CheckoutLine, Discount, Product, Sale, StoreConfig, Variant }
+export { db }
